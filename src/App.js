2baad582f5e4fe25d9407eff1265cb479dc1c8c8
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2022-10-19 17:00:38 +0330

    hash router used

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Main from "./components/MainComponent.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Main />
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
